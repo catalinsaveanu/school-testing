a54aac02ba6bec51a16443dd06fc10ffc923ff64
Catalin Saveanu <catalinsaveanu@gmail.com>
2018-11-07 00:40:54 +0200

    save new test wtihout problems

--- src/store/actions.js
+++ src/store/actions.js
@@ -6,74 +6,122 @@ export default {
     context.commit('setUser', user);
   },
   signup({ commit }, user) {
-    return firebase.auth().createUserWithEmailAndPassword(user.email, user.password).then(
-      (firebaseUser) => {
-        const db = firebase.firestore();
+    return firebase
+      .auth()
+      .createUserWithEmailAndPassword(user.email, user.password)
+      .then(
+        (firebaseUser) => {
+          const db = firebase.firestore();
 
-        return db.collection('users').doc(firebaseUser.user.uid).set({
-          email: user.email,
-          name: user.name,
-          role: user.role
-        });
-      },
-      (err) => {
-        console.log(err);
-      }
-    ).then((doc) => {
-      console.log(doc);
-      commit('setUser', user);
-    });
+          return db
+            .collection('users')
+            .doc(firebaseUser.user.uid)
+            .set({
+              email: user.email,
+              name: user.name,
+              role: user.role
+            });
+        },
+        (err) => {
+          console.log(err);
+        }
+      )
+      .then((doc) => {
+        console.log(doc);
+        commit('setUser', user);
+      });
   },
   login({ commit }, user) {
-    return firebase.auth().signInWithEmailAndPassword(user.email, user.password).then(
-      (firebaseUser) => {
-        const db = firebase.firestore();
+    return firebase
+      .auth()
+      .signInWithEmailAndPassword(user.email, user.password)
+      .then(
+        (firebaseUser) => {
+          const db = firebase.firestore();
 
-        return db.collection('users').doc(firebaseUser.user.uid).get();
-      },
-      (err) => {
-        console.log(err);
-      }
-    ).then((doc) => {
-      commit('setUser', doc.data());
-    });
+          return db
+            .collection('users')
+            .doc(firebaseUser.user.uid)
+            .get();
+        },
+        (err) => {
+          console.log(err);
+        }
+      )
+      .then((doc) => {
+        commit('setUser', doc.data());
+      });
   },
   loadTests({ commit }) {
     // eslint-disable-next-line
     let tests = [];
     const db = firebase.firestore();
 
-    db.collection('tests').get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        tests.push({ id: doc.id, ...doc.data() });
-      });
+    db.collection('tests')
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          tests.push({ id: doc.id, ...doc.data() });
+        });
 
-      commit('setTests', tests);
-    });
+        commit('setTests', tests);
+      });
+  },
+  saveTest({ commit }, test) {
+    const db = firebase.firestore(),
+      testCollection = db.collection('tests');
+
+    let actionRef;
+
+    if (test.id) {
+      actionRef = testCollection.update(test);
+    } else {
+      actionRef = testCollection.add(test);
+    }
+
+    return actionRef
+      .then(() => {
+        commit('setAlert', {
+          show: true,
+          color: '#00FF00',
+          message: 'Testul a fost salvat!'
+        });
+        // eslint-disable-next-line
+      })
+      .catch((error) => {
+        commit('setAlert', {
+          show: true,
+          color: '#FF0000',
+          message: `Eroare! Testul nu a fost salvat! ${error}`
+        });
+      });
   },
   deleteTest({ commit }, test) {
     const db = firebase.firestore();
 
-    db.collection('tests').doc(test.id).set({
-      ...test,
-      deleted: true
-    }).then(() => {
-      commit('setAlert', {
-        show: true,
-        color: '#00FF00',
-        message: 'Testul a fost sters!'
+    db.collection('tests')
+      .doc(test.id)
+      .set({
+        ...test,
+        deleted: true
+      })
+      .then(() => {
+        commit('setAlert', {
+          show: true,
+          color: '#00FF00',
+          message: 'Testul a fost sters!'
+        });
+        // eslint-disable-next-line
+      })
+      .catch((error) => {
+        commit('setAlert', {
+          show: true,
+          color: '#FF0000',
+          message: `Eroare! Testul nu a fost sters! ${error}`
+        });
       });
-    // eslint-disable-next-line
-    }).catch((error) => {
-      commit('setAlert', {
-        show: true,
-        color: '#FF0000',
-        message: `Eroare! Testul nu a fost sters! ${error}`
-      });
-    });
   },
   updateAlert({ commit }, alert) {
     commit('setAlert', alert);
   }
 };
-
--- src/views/AddTest.vue
+++ src/views/AddTest.vue
@@ -38,38 +38,107 @@
           ></v-text-field>
           <v-checkbox
             v-model="active"
-            value="true"
             label="Activ"
-            type="active"
           ></v-checkbox>
         </v-flex>
-        <div class="separator">
-            <v-btn
-              absolute
-              dark
-              fab
-              top
-              right
-              color="pink"
-              @click="addQuestion"
-            >
-              <v-icon>add</v-icon>
+        <v-toolbar flat color="white">
+          <v-toolbar-title>Probleme</v-toolbar-title>
+          <v-divider
+            class="mx-2"
+            inset
+            vertical
+          ></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog v-model="dialog" max-width="600px">
+            <v-btn fab dark color="indigo" slot="activator" class="mb-2">
+              <v-icon dark>add</v-icon>
             </v-btn>
-        </div>
-        <v-flex>
-        <Problem
-            v-for="(problem, index) in problems"
-            :key="index"
-            :question="problem.question"
-            :answers="problem.answers"
-            :correct-answer="problem.correctAnswer"
-            :problem-number="problem.problemNumber"
+            <v-card>
+              <v-card-title>
+                <span class="headline">{{ formTitle }}</span>
+              </v-card-title>
+
+              <v-card-text>
+                <v-container grid-list-md>
+                  <v-layout wrap>
+                    <v-flex xs12>
+                      <v-text-field label="Intrebare*" required v-model="editedItem.question"></v-text-field>
+                    </v-flex>
+                    <v-radio-group v-model="editedItem.correctAnswer" row required >
+                      <v-flex class="radio-cell xs12 sm6 md6 ">
+                        <v-layout align-center>
+                          <v-radio value="0"></v-radio>
+                          <v-text-field v-model="editedItem.answerA" label="(A)"></v-text-field>
+                        </v-layout>
+                      </v-flex>
+                      <v-flex class="radio-cell xs12 sm6 md6 ">
+                        <v-layout align-center>
+                          <v-radio value="1"></v-radio>
+                          <v-text-field v-model="editedItem.answerB" label="(B)"></v-text-field>
+                        </v-layout>
+                      </v-flex>
+                      <v-flex class="radio-cell xs12 sm6 md6 ">
+                        <v-layout align-center>
+                          <v-radio value="2"></v-radio>
+                          <v-text-field v-model="editedItem.answerC" label="(C)"></v-text-field>
+                        </v-layout>
+                      </v-flex>
+                      <v-flex class="radio-cell xs12 sm6 md6 ">
+                        <v-layout align-center>
+                          <v-radio value="3"></v-radio>
+                          <v-text-field v-model="editedItem.answerD" label="(D)"></v-text-field>
+                        </v-layout>
+                      </v-flex>
+                    </v-radio-group>
+                  </v-layout>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" flat @click.native="close">Cancel</v-btn>
+                <v-btn color="blue darken-1" flat @click.native="save">Save</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+        <v-data-table
+          :headers="headers"
+          :items="problems"
+          hide-actions
+          class="elevation-1"
         >
-        </Problem>
-        </v-flex>
+          <template slot="items" slot-scope="props">
+            <td>{{ props.item.position }}.</td>
+            <td>{{ props.item.question }}</td>
+            <td>{{ props.item.answerA }}</td>
+            <td>{{ props.item.answerB }}</td>
+            <td>{{ props.item.answerC }}</td>
+            <td>{{ props.item.answerD }}</td>
+            <td class="text-xs-center">{{ props.item.correctAnswer }}</td>
+            <td class="justify-center layout align-center">
+              <v-icon
+                small
+                class="mr-2"
+                @click="editItem(props.item)"
+              >
+                edit
+              </v-icon>
+              <v-icon
+                small
+                @click="deleteItem(props.item)"
+              >
+                delete
+              </v-icon>
+            </td>
+          </template>
+          <template slot="no-data">
+            Nu a fost adaugata nici o problema. Apasa pe butonul de mai sus pentru a adauga o problema.
+          </template>
+        </v-data-table>
         <v-spacer column></v-spacer>
         <v-flex>
-          <v-btn color="error" @click="submit">Renunta</v-btn>
+          <v-btn color="error" @click="cancel">Renunta</v-btn>
           <v-btn color="success" @click="submit">Adauga</v-btn>
         </v-flex>
       </v-layout>
@@ -79,15 +148,14 @@
 
 <script>
 import 'firebase/firestore';
-import Problem from './../components/Problem.vue';
+import confirm from 'vuetify';
+
+import router from './../router/';
 
 export default {
   name: 'UserDashboard',
-  components: {
-    Problem
-  },
-  mounted() {
-  },
+  components: {},
+  mounted() {},
   data() {
     return {
       menu1: false,
@@ -95,29 +163,151 @@ export default {
       subject: 'romana',
       subjects: ['romana', 'matematica'],
       description: '',
-      active: false,
+      active: true,
       submitStatus: null,
-      problems: []
+      problems: [],
+      dialog: false,
+      headers: [
+        {
+          text: 'Numar',
+          align: 'left',
+          sortable: true,
+          value: 'position'
+        },
+        { text: 'Intrebarea', value: 'question', sortable: false },
+        { text: '(A)', value: 'answerA', sortable: false },
+        { text: '(B)', value: 'answerB', sortable: false },
+        { text: '(C)', value: 'answerC', sortable: false },
+        { text: '(D)', value: 'answerD', sortable: false },
+        {
+          text: 'Raspuns',
+          value: 'correctAnswer',
+          sortable: false,
+          align: 'center'
+        },
+        { text: 'Actiuni', sortable: false, align: 'center' }
+      ],
+      editedIndex: -1,
+      editedItem: {
+        position: 0,
+        question: '',
+        answerA: '',
+        answerB: '',
+        answerC: '',
+        answerD: '',
+        correctAnswer: 0
+      },
+      defaultItem: {
+        position: 0,
+        question: '',
+        answerA: '',
+        answerB: '',
+        answerC: '',
+        answerD: '',
+        correctAnswer: 0
+      }
     };
   },
   computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? 'Adauga Problema' : 'Modifica Problema';
+    }
+  },
+  watch: {
+    dialog(val) {
+      return val || this.close();
+    }
   },
   methods: {
-    addQuestion() {
-      this.problems.push({
-        question: 'Intrebarea...',
-        answers: ['', '', '', ''],
-        correctAnswer: 0,
-        problemNumber: this.problems.length + 1
-      });
+    editItem(item) {
+      this.editedIndex = this.problems.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
     },
+
+    deleteItem(item) {
+      const index = this.problems.indexOf(item);
+
+      if (confirm('Esti sigur ca vrei sa stergi aceasta problema?')) {
+        this.problems.splice(index, 1);
+      }
+    },
+
+    close() {
+      this.dialog = false;
+
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      }, 300);
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.problems[this.editedIndex], this.editedItem);
+      } else {
+        this.problems.push({
+          ...this.editedItem,
+          position: this.problems.length + 1
+        });
+      }
+
+      this.close();
+    },
+
+    cancel() {
+      router.go(-1);
+    },
+
     submit() {
+      // eslint-disable-next-line
+      const problems = this.problems.map((problem) => {
+          return {
+            correct_answer: problem.correctAnswer,
+            question: problem.question,
+            answers: [
+              problem.answerA,
+              problem.answerB,
+              problem.answerC,
+              problem.answerD
+            ]
+          };
+        }),
+        test = {
+          active: this.active,
+          date: this.date,
+          deleted: false,
+          description: this.description,
+          subject: this.subject,
+          problems
+        };
+
+      this.$store.dispatch('saveTest', test).then(() => {
+        this.$router.go(-1);
+      });
     }
   }
 };
 </script>
 
 <style scoped>
-  form {height: 100%; width: 100%}
-  .separator {border-top: 1px dashed gray; position: relative}
+form {
+  height: 100%;
+  width: 100%;
+}
+.separator {
+  border-top: 1px dashed gray;
+  position: relative;
+}
+.v-dialog .v-radio {
+  margin-right: 0;
+}
+
+.container.grid-list-md .layout .flex.radio-cell:nth-child(2n) {
+  padding-left: 10px;
+}
+
+.container.grid-list-md .layout .flex.radio-cell:nth-child(2n + 1) {
+  padding-right: 10px;
+}
 </style>
