732c56b775f680e622f17fbc50bf778506f2c3df
Catalin Saveanu <catalinsaveanu@gmail.com>
2018-11-20 00:47:13 +0200

    update take test

--- src/views/TakeTest.vue
+++ src/views/TakeTest.vue
@@ -1,60 +1,61 @@
 <template>
+<v-flex xs12 fill-height>
   <v-card
-      class="mx-auto"
+      class="mx-auto fill-height"
       max-width="1200"
     >
-    <v-window v-model="currentTest">
-      <v-window-item
-        v-for="(problem, index) in problems"
-        :key="`card-${index}`"
-      >
-
-        <v-card-title class="title font-weight-regular justify-space-between">
-          <span>{{ problem.question }}</span>
-          <v-avatar
-            color="primary lighten-2"
-            class="subheading white--text"
-            size="24"
-            v-text="index"
-          ></v-avatar>
-        </v-card-title>
-      </v-window-item>
-    </v-window>
-
-    <v-card-actions class="justify-space-between">
-      <v-btn
-        flat
-        @click="prev"
-      >
-        <v-icon>mdi-chevron-left</v-icon>
+    <v-layout justify-space-between column fill-height>
+      <v-btn color="primary" fab small dark>
+        <v-icon>chevron_left</v-icon>
       </v-btn>
-      <v-item-group
-        v-model="currentProblem"
-        class="text-xs-center"
-        mandatory
-      >
-        <v-item
-          v-for="n in problems.length"
-          :key="`btn-${n}`"
+      <v-window
+        class="fill-height"
+        v-model="currentProblem">
+        <v-window-item
+          v-for="(problem, index) in problems"
+          :key="index"
         >
-          <v-btn
-            slot-scope="{ active, toggle }"
-            :input-value="active"
-            icon
-            @click="toggle"
-          >
-            <v-icon>mdi-record</v-icon>
-          </v-btn>
-        </v-item>
-      </v-item-group>
-      <v-btn
-        flat
-        @click="next"
-      >
-        <v-icon>mdi-chevron-right</v-icon>
-      </v-btn>
-    </v-card-actions>
+          <v-card-title class="title font-weight-regular justify-space-between">
+            <span>{{ problem.question }}</span>
+          </v-card-title>
+
+          <v-layout row wrap>
+            <v-flex
+              class="xs6"
+              v-for="(value, index2) in DEFAULT_ANSWERS"
+              :key="`answer-${index2}`">
+              <v-card dark color="secondary">
+                <v-card-text class="px-0">{{value}}) {{problem[`answer${value}`]}}</v-card-text>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-window-item>
+      </v-window>
+
+      <v-card-actions class="justify-center">
+        <v-btn
+          color="primary"
+          fab
+          small
+          dark
+          @click="prev"
+        >
+          <v-icon>chevron_left</v-icon>
+        </v-btn>
+        <div class="current-problem-position">
+          {{currentProblem + 1}} din {{problems.length}}
+        </div>
+        <v-btn color="primary"
+          fab
+          small
+          dark
+          @click="next">
+          <v-icon dark>chevron_right</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-layout>
   </v-card>
+</v-flex>
 </template>
 
 <script>
@@ -83,7 +84,8 @@ export default {
   },
   data() {
     return {
-      currentProblem: 0
+      currentProblem: 0,
+      DEFAULT_ANSWERS: ['A', 'B', 'C', 'D']
     };
   },
   computed: {
@@ -96,16 +98,26 @@ export default {
   },
   methods: {
     next() {
-      this.currentProblem =
-        this.currentProblem + 1 === length ? 0 : this.currentProblem + 1;
+      this.currentProblem = this.currentProblem + 1;
+      if (this.currentProblem > this.problems.length - 1) {
+        this.currentProblem = this.problems.length - 1;
+      }
     },
     prev() {
-      this.currentProblem =
-        this.currentProblem - 1 < 0 ? this.length - 1 : this.currentProblem - 1;
+      this.currentProblem = this.currentProblem - 1;
+      if (this.currentProblem < 0) {
+        this.currentProblem = 0;
+      }
     }
   }
 };
 </script>
 
 <style scoped>
+.current-problem-position {
+  padding: 0 10px;
+}
+.v-card__actions {
+  background: #fff8e1;
+}
 </style>
